79094471704f126ec382518881f8cb990d4157da
shalominy <shalominy@graduate.utm.my>
2025-01-07 23:05:43 +0800

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Healio',
+    version= '0.0.0',
+    author= 'Shalu',
+    author_email= 'shalominy@graduate.utm.my',
+    packages= find_packages(),
+    install_requires = []
+
+)
